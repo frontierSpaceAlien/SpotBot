bab05d9d1f0a738721131243d20b63d965ee479b
das <104743984+frontierSpaceAlien@users.noreply.github.com>
2025-12-10 14:51:50 +1300

    added nitro

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import { devtools } from '@tanstack/devtools-vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import { nitro } from 'nitro/vite'
 import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
@@ -17,7 +18,9 @@ const config = defineConfig({
     tailwindcss(),
     tanstackStart(),
     viteReact(),
+    nitro(),
   ],
+  nitro: {},
 })
 
 export default config
